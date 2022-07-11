f7e07b974419662ce7945e49b1aca39bf402eb3b
Natalie Goodwin <nmgoodwin27@gmail.com>
2022-07-10 23:03:04 -0500

    first commit July 10

---
+++ src/house.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class House extends React.Component {
+ render() {
+    const rooms = this.props.data.rooms
+    ? this.props.data.rooms.map((room, index) => 
+        <li 
+    )
+ }
+} /* house will use new room form we will create later
+but it isn't created yet */
